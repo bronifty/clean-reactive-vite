dbd40febd0bb5d3a71b54284a65751078d20b8e8
bronifty <bronifty@gmail.com>
2023-09-04 04:10:03 -0400

    class-v-object-this-lexical-v-caller-scope

--- debug/objectThis.ts
+++ debug/objectThis.ts
@@ -8,6 +8,7 @@ const obj = {
       console.log(`function this: ${JSON.stringify(this, null, 2)}`);
     }
     innerFunction.apply({ context: "Custom Context" }, ["Inner Local"]);
+    innerFunction("Inner Local");
     const innerFunctionArrow = (innerVariable) => {
       console.log(`arrow Outer Variable: ${outerVariable}`);
       console.log(`arrow Outer Local Variable: ${outerLocalVariable}`);
@@ -17,4 +18,51 @@ const obj = {
     innerFunctionArrow("Inner Local");
   },
 };
-obj.outerFunction("Outer Global");
+// obj.outerFunction("Outer Global");
+
+const obj2 = {
+  prop: "I am a property of obj2",
+  regularFunction: function () {
+    console.log(
+      "Inside regularFunction, this is:",
+      JSON.stringify(this, null, 2)
+    );
+  },
+  arrowFunction: () => {
+    console.log(
+      "Inside arrowFunction, this is:",
+      JSON.stringify(this, null, 2)
+    );
+  },
+};
+const obj3 = {
+  prop: "I am a property of obj3",
+  regularFunction: function () {
+    console.log(
+      "Inside regularFunction, this is:",
+      JSON.stringify(this, null, 2)
+    );
+  },
+  arrowFunction: () => {
+    console.log(
+      "Inside arrowFunction, JSON.stringify(this, null, 2) is:",
+      JSON.stringify(this, null, 2)
+    );
+  },
+  nestedFunction: function () {
+    const arrowFunctionInside = () => {
+      console.log(
+        "Inside arrowFunctionInside, JSON.stringify(this, null, 2) is:",
+        JSON.stringify(this, null, 2)
+      );
+    };
+    arrowFunctionInside();
+  },
+};
+
+obj3.regularFunction();
+obj3.arrowFunction();
+obj3.nestedFunction();
+
+// obj2.regularFunction();
+// obj2.arrowFunction();
